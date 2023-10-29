0a6479122c7503ce061ac38aaed0776f63f70a9f
Alejandro-M-Cruz <alejandromatiascc@gmail.com>
2023-10-29 13:05:40 +0000

    Refactored to use MoveDifferential instead of using motors separately

--- square/main.py
+++ square/main.py
@@ -1,51 +1,38 @@
 #!/usr/bin/env python3
 
-import math
-from functools import partial
+from ev3dev2.motor import LargeMotor, OUTPUT_B, OUTPUT_C, MoveDifferential
+from ev3dev2.wheel import EV3EducationSetTire
 
-from ev3dev2.motor import LargeMotor, OUTPUT_B, OUTPUT_C
+WHEEL_DISTANCE_MM = 118
+MOTOR_SPEED_PERCENT = 40
+MOTOR_SPEED_PERCENT_WHEN_TURNING = 15
 
-WHEEL_DIAMETER_IN_CENTIMETRES = 5.6
-AXLE_IN_CENTIMETERS = 11.8
-AXLE_CIRCUMFERENCE_IN_CENTIMETERS = AXLE_IN_CENTIMETERS * math.pi
-MOTOR_SPEED_PERCENT = 25
-MOTOR_SPEED_PERCENT_WHEN_TURNING = 10
-
-left_motor = LargeMotor(OUTPUT_B)
-right_motor = LargeMotor(OUTPUT_C)
-
-
-def move_left_motor(speed: float, rotations: float, block=True, brake=True):
-    left_motor.on_for_rotations(speed=speed, rotations=rotations, block=block, brake=brake)
+move_differential = MoveDifferential(
+    left_motor_port=OUTPUT_B,
+    right_motor_port=OUTPUT_C,
+    wheel_class=EV3EducationSetTire,
+    wheel_distance_mm=WHEEL_DISTANCE_MM,
+    motor_class=LargeMotor
+)
 
 
-def move_right_motor(speed: float, rotations: float, block=True, brake=True):
-    right_motor.on_for_rotations(speed=speed, rotations=rotations, block=block, brake=brake)
+def move_forward(distance_cm: float, speed=MOTOR_SPEED_PERCENT, brake=True):
+    move_differential.on_for_distance(distance_mm=distance_cm * 10, speed=speed, brake=brake)
 
 
-def move_forward(speed: float, centimeters: float, brake=True):
-    rotations = centimeters / (WHEEL_DIAMETER_IN_CENTIMETRES * math.pi)
-    move_left_motor(speed=speed, rotations=rotations, block=False, brake=brake)
-    move_right_motor(speed=speed, rotations=rotations, brake=brake)
+def turn_left(degrees: float, speed=MOTOR_SPEED_PERCENT, brake=True):
+    move_differential.turn_left(degrees=degrees, speed=speed, brake=brake)
 
 
-def turn(speed: float, degrees: float):
-    rotations = (AXLE_CIRCUMFERENCE_IN_CENTIMETERS * degrees / 360) / (WHEEL_DIAMETER_IN_CENTIMETRES * math.pi)
-    move_left_motor(speed=speed, rotations=-rotations, block=False)
-    move_right_motor(speed=speed, rotations=rotations)
+def turn_right(degrees: float, speed=MOTOR_SPEED_PERCENT, brake=True):
+    move_differential.turn_right(degrees=degrees, speed=speed, brake=brake)
 
 
-move_forward_one_meter = partial(move_forward, speed=MOTOR_SPEED_PERCENT, centimeters=100)
-turn_left_90_degrees = partial(turn, speed=MOTOR_SPEED_PERCENT_WHEN_TURNING, degrees=90)
-
-
-def draw_one_meter_square():
+def move_square(side_length_cm: float):
     for _ in range(4):
-        move_forward_one_meter()
-        turn_left_90_degrees()
+        move_forward(distance_cm=side_length_cm)
+        turn_left(degrees=90)
 
 
 if __name__ == '__main__':
-    draw_one_meter_square()
-    left_motor.off()
-    right_motor.off()
+    move_square(side_length_cm=100)
