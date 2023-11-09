d35f1a66c86f0029a456402e7b539981667f08b3
Alejandro-M-Cruz <alejandromatiascc@gmail.com>
2023-11-09 10:26:06 +0000

    Introduced logging

--- square/main.py
+++ square/main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import logging
+from time import sleep
+
 from ev3dev2.motor import LargeMotor, OUTPUT_B, OUTPUT_C, MoveDifferential
 from ev3dev2.wheel import EV3EducationSetTire
 
@@ -14,6 +17,13 @@ move_differential = MoveDifferential(
     wheel_distance_mm=WHEEL_DISTANCE_MM,
     motor_class=LargeMotor
 )
+
+logging.basicConfig(filename='robot.log', level=logging.INFO)
+
+
+def log_position():
+    logging.info('left_motor: %s', move_differential.left_motor.position)
+    logging.info('right_motor: %s', move_differential.right_motor.position)
 
 
 def move_forward(distance_cm: float, speed=MOTOR_SPEED_PERCENT, brake=True):
@@ -30,15 +40,22 @@ def turn_right(degrees: float, speed=MOTOR_SPEED_PERCENT_WHEN_TURNING, brake=Tru
 
 def move_square(side_length_cm: float, laps: int = 1, direction: str = 'clockwise'):
     for _ in range(4 * laps):
+        sleep(0.5)
         move_forward(distance_cm=side_length_cm)
+        log_position()
+        sleep(0.5)
         if direction == 'clockwise':
             turn_right(degrees=90)
         elif direction == 'counterclockwise':
             turn_left(degrees=90)
         else:
             raise ValueError('direction must be either clockwise or counterclockwise')
+        log_position()
 
 
 if __name__ == '__main__':
-    move_square(side_length_cm=100, laps=10)
-    move_square(side_length_cm=100, laps=10, direction='counterclockwise')
+    # move_square(side_length_cm=100, laps=10)
+    # move_square(side_length_cm=100, laps=10, direction='counterclockwise')
+    log_position()
+    move_square(side_length_cm=100)
+    logging.info('Finished square')
